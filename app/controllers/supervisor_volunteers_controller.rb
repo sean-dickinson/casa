84522f99f4b546db3ab9d2e88ed0ba935b9e2585
SHAMI_TOMITA <shamitomita@gmail.com>
2023-06-28 16:34:04 -0400

    Bulk Commit
    Co-authored-by: Eric Halverson <elhalvers@users.noreply.github.com>

--- app/controllers/supervisor_volunteers_controller.rb
+++ app/controllers/supervisor_volunteers_controller.rb
@@ -25,10 +25,27 @@ class SupervisorVolunteersController < ApplicationController
     redirect_to request.referrer, notice: flash_message
   end
 
+  def bulk_assignment
+    authorize :supervisor_volunteer
+    volunteer_ids = params[:volunteer_ids]
+    supervisor = supervisor_volunteer_parent
+    created_volunteers = ""
+    volunteer_ids.each do |vol_id|
+      supervisor_volunteer =  supervisor_volunteer_parent.supervisor_volunteers.find_or_create_by!(volunteer_id: vol_id.to_i)
+      supervisor_volunteer.is_active = true unless supervisor_volunteer&.is_active?
+      volunteer = supervisor_volunteer.volunteer 
+      supervisor_volunteer.save
+      created_volunteers += "#{volunteer.display_name}, " #Take into account single assignments and give multiple assignments proper format 
+    end 
+    flash_message = "#{created_volunteers} successfully assigned to #{supervisor.display_name}"
+    
+    redirect_to request.referrer, notice: flash_message
+  end 
+
   private
 
   def supervisor_volunteer_params
-    params.require(:supervisor_volunteer).permit(:supervisor_id, :volunteer_id)
+    params.require(:supervisor_volunteer).permit(:supervisor_id, :volunteer_id, :volunteer_ids)
   end
 
   def supervisor_volunteer_parent
